7e756ad7e85285bf7106e5a3cd281a653bd7c831
neil4droid <cterse@gmail.com>
2021-06-10 21:20:02 -0400

    Menubar and FBXLoader changes

--- core/static/assets/js/Menubar.js
+++ core/static/assets/js/Menubar.js
@@ -17,10 +17,10 @@ function Menubar( editor ) {
 	container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
 	container.add( new MenubarAdd( editor ) );
-	container.add( new MenubarPlay( editor ) );
+	// container.add( new MenubarPlay( editor ) );
 	container.add( new MenubarExamples( editor ) );
 	container.add( new MenubarView( editor ) );
-	container.add( new MenubarHelp( editor ) );
+	// container.add( new MenubarHelp( editor ) );
 
 	container.add( new MenubarStatus( editor ) );
 
