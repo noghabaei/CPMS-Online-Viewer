1c4c686496992b901746b764f00f8e0c5057b1e1
neil4droid <cterse@gmail.com>
2021-06-15 19:16:18 -0400

    Prepare Element View Canvas

--- core/static/assets/three/editor/js/Sidebar.Panel.Element.js
+++ core/static/assets/three/editor/js/Sidebar.Panel.Element.js
@@ -1,8 +1,12 @@
+import * as THREE from '/static/assets/three/build/three.module.js';
+import * as EV from '/static/assets/js/element-view.js';
 import { UIBreak, UIButton, UICanvas, UIPanel, UIRow, UISelect, UIText } from "./libs/ui.js";
 
 function SidebarElementPanel( editor ) {
-    
+
     var strings = editor.strings;
+
+    var elementScene, elementCamera, elementRenderer; 
 
     var container = new UIPanel();
 	container.setBorderTop( '0' );
@@ -12,12 +16,24 @@ function SidebarElementPanel( editor ) {
     var buttonRow = new UIRow();
     var compatibilityCheckButton = new UIButton( 'Compatibility Check' );   // EXTERNALIZE
     compatibilityCheckButton.setId( 'compat-btn' );
+
+    compatibilityCheckButton.onClick( function( e ) {
+        document.getElementById( 'view-mode-btn' ).style.display = 'block';
+        e.target.style.display = 'none';
+    } );
+
     buttonRow.add( compatibilityCheckButton );
     
     // View mode button
     var viewModeButton = new UIButton( 'View Mode' ); // EXTERNALIZE
     viewModeButton.setId( 'view-mode-btn' );
     viewModeButton.setStyle( 'display' , ['none'] );
+
+    viewModeButton.onClick( function( e ) {
+        document.getElementById( 'compat-btn' ).style.display = 'block';
+        e.target.style.display = 'none';
+    } );
+
     buttonRow.add( viewModeButton );
 
     container.add( buttonRow );
@@ -25,15 +41,28 @@ function SidebarElementPanel( editor ) {
     // Elements dropdown
     var selectRow = new UIRow();
     var elementSelect = new UISelect().setOptions( {
-        'None': 'No Elements Loaded'
+        '': 'No Elements Loaded',
+        'Test Object 1 Type': 'Object 1 ID',
+        'Test Object 2 Type': 'Object 2 ID'
     } );
     elementSelect.setWidth( '200px' );
+    elementSelect.onChange( function( e ) {
+        // document.getElementById( 'element-info-text' )
+        elementInfo.setValue( e.target.value );
+
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        var cube = new THREE.Mesh( geometry, material );
+        EV.loadElementObject( cube, false );
+
+    } );
     selectRow.add( elementSelect );
     container.add( selectRow );
 
     // Info string
     var elementInfoRow = new UIRow();
-	var elementInfo = new UIText(' - ');
+
+	var elementInfo = new UIText('').setId( 'element-info-text' );
 
 	elementInfoRow.add( new UIText( 'Info:' ).setWidth( '90px' ) );
 	elementInfoRow.add( elementInfo );
@@ -48,5 +77,6 @@ function SidebarElementPanel( editor ) {
 
     return container;
 }
+
 
 export { SidebarElementPanel };
