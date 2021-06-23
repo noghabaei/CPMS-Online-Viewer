ff3b4fb83536948c5ef126362ec2a438c98b85ab
cterse <cterse@gmail.com>
2021-06-23 14:52:20 -0400

    Integrate Controls panel in Editor

--- core/static/assets/three/editor/js/Sidebar.js
+++ core/static/assets/three/editor/js/Sidebar.js
@@ -7,6 +7,7 @@ import { SidebarAnimation } from './Sidebar.Animation.js';
 import { SidebarProject } from './Sidebar.Project.js';
 import { SidebarSettings } from './Sidebar.Settings.js';
 import { SidebarElementPanel } from "./Sidebar.Panel.Element.js";
+import { SidebarControls } from './Sidebar.Controls.js';
 
 function Sidebar( editor ) {
 
@@ -24,11 +25,13 @@ function Sidebar( editor ) {
 	var project = new SidebarProject( editor );
 	var settings = new SidebarSettings( editor );
 	var elementPanel = new SidebarElementPanel( editor );
+	var controlsPanel = new SidebarControls( editor );
 
 	container.addTab( 'scene', strings.getKey( 'sidebar/scene' ), scene );
 	container.addTab( 'project', strings.getKey( 'sidebar/project' ), project );
 	container.addTab( 'settings', strings.getKey( 'sidebar/settings' ), settings );
 	container.addTab( 'element-panel', 'Element View', elementPanel );
+	container.addTab( 'controls', 'Controls', controlsPanel );
 	container.select( 'scene' );
 
 	return container;
